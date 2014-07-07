060aad173a7cac31501a0a579052587576272fdb
Andrew Tillett <AndrewD.Tillett@gmail.com>
2014-07-07 01:19:01 -0400

    Intro to Python - Com, Var, Print Examples

--- Intro to Python/main.py
+++ Intro to Python/main.py
@@ -1,1 +1,20 @@
-print "hello world!"
+#one lined comments
+'''
+Doc strings
+'''
+
+
+first_name = "Kermit"
+last_name = "De Frog"
+
+#response = raw_input("Enter Your Name   ")
+#print "Hello there,", response
+
+birth_year = 1992
+current_year = 2014
+age = current_year - birth_year
+print "You are " + str(age) + " years old"
+
+#Assignment Operators JS (++ --) Python does not have these
+#Python (+=, -=, *=, /+)
+
