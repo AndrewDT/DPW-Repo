077efdc51f4a2539d44b272e6a4c2bd639673edd
Andrew Tillett <AndrewD.Tillett@gmail.com>
2014-07-07 01:33:03 -0400

    Intro to Python: Cond. Coll. Examples

--- Intro to Python/main.py
+++ Intro to Python/main.py
@@ -13,8 +13,31 @@ last_name = "De Frog"
 birth_year = 1992
 current_year = 2014
 age = current_year - birth_year
-print "You are " + str(age) + " years old"
+#print "You are " + str(age) + " years old"
 
 #Assignment Operators JS (++ --) Python does not have these
 #Python (+=, -=, *=, /+)
+
+'''
+budget = 51
+
+if budget > 100:
+    brand = "Nike"
+    print "Yay! we can buy cool " + brand + " shoes!"
+elif budget > 50:
+    print "We can at least get some generic sneakers."
+else:
+    print "No cool shoes for me."
+
+'''
+
+characters = ["leia", "luke", "chewy", "lando"]
+characters.append("obi wan")
+#print characters
+
+movies = dict() #Create dictionary object
+movies = {"Star Wars":"Darth Vader", "Silence of the Lambs":"Hannibal Lecter"}
+print movies["Star Wars"]
+
+
 
