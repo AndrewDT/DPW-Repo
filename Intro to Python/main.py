157a903e35d524eb122d987754dd7762aa2da1bf
Andrew Tillett <AndrewD.Tillett@gmail.com>
2014-07-07 01:51:34 -0400

    Intro to Python: Loop Functions Examples

--- Intro to Python/main.py
+++ Intro to Python/main.py
@@ -31,13 +31,78 @@ else:
 
 '''
 
+#ARRAY----------------------
 characters = ["leia", "luke", "chewy", "lando"]
 characters.append("obi wan")
 #print characters
 
+#DICTIONARY----------------------------
 movies = dict() #Create dictionary object
 movies = {"Star Wars":"Darth Vader", "Silence of the Lambs":"Hannibal Lecter"}
-print movies["Star Wars"]
+#print movies["Star Wars"]
+
+#Loops and Functions
+
+'''
+#While Loop------
+i = 0
+while i<9:
+    #print "The count is", i
+    i = i+1
+
+#For Loop------
+for i in range(0,10):
+    print "The count is", i
+    i = i+1
+
+#"FOR EACH" LOOP ------------
+rappers = ["Tupac", "Nas", "Biggie Smalls"]
+for r in rappers:
+    print "One of the best rappers is " + r
+'''
+
+#FUNCTIONS IN PYTHON -------------------------
+#def means DEFINITION
+
+x = 2
+
+def calcArea(h, w):
+    area = h * w
+    return area + x
+
+a = calcArea(20, 40)
+#print "My area is " + str(a) + "sqft"
+#print a
+
+
+title = "Contact Us"
+body = "You can contact us at contact@us.com"
+message = '''
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>{title}</title>
+    </head>
+    <body>
+        {body}
+    </body>
+</html>
+'''
+# **locals() method replaces brackets {variable} with corresponding value
+message = message.format(**locals())
+print message
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
