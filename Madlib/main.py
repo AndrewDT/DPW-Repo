8956b175d731749dd9efedbb0565b6ee282d245f
Andrew Tillett <AndrewD.Tillett@gmail.com>
2014-07-08 20:51:33 -0400

    Madlib instructions and story added(comments)

---
+++ Madlib/main.py
@@ -0,0 +1,41 @@
+'''
+Name: Andrew Tillett
+Class: Design Patters for Web Programming 1407
+Assignment: Madlib
+Date: 7/7/14
+
+MadLib
+
+Mad Libs is a comic word game where one player is prompted for a list of words to fill in the blanks of a story.
+
+Create your own mad lib in Python that collects (at least) the following information:
+
+HAS TO COLLECT
+At least 3 strings
+At least 3 numbers
+And includes the following elements:
+
+CAN COLLECT FROM
+One array
+One dictionary
+
+At least 2 mathematical operators
+Two conditional statements
+(with at least one logical operator)
+At least one function
+It must return a value
+It must have and use parameters
+One loop - Remember that this is for repeating code!
+And meet the following requirements:
+
+The mad lib's code must be well commented and include the appropriate use of Docstrings
+All elements should have a purpose within the greater madlib. (In other words you can't create an array or function that isn't used)
+You must make at least 12 meaningful commits to this file in your repository or your assignment will earn a 0.
+Any errors will earn an automatic ZERO on this assignment.
+Your mad lib should print out the entire story using the "print" function
+
+A villain was attacking the (STRING). In no time, he had caused insurmountable damage while terrorizing the citizens.
+Luckily, (STRING) showed up in time to stop the madman! They fought and struggled for (NUMBER) minutes, but it was clear who the victor would be.
+As a reward, the hero was given (NUMBER) (STRING) and the villain was given (NUMBER) years in prison!
+
+'''
