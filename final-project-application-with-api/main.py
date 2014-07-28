bfdd9f4ebc922cf205ea7949616bf7079c8263ec
Andrew Tillett <AndrewD.Tillett@gmail.com>
2014-07-28 13:55:42 -0400

    Final Project
    Page Abstract Class created:
    Included properties and methods:
    self._head
    self._body
    self._close
    def page_write

--- final-project-application-with-api/main.py
+++ final-project-application-with-api/main.py
@@ -6,10 +6,36 @@ Assignment: Final Project: Application with API
 
 '''
 import webapp2
+import urllib2 #gives us python classes and code needed to requesting info, receiving, and opening
+import json
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
+
+
+
+class Page(object):
+    def __init__(self):
+        self._head = '''
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Book Search App</title>
+    </head>
+    <body>
+        '''
+        self._body = "Book Searching Application"
+        self._close = '''
+
+    </body>
+</html>
+        '''
+
+    def page_write(self):
+        return self._head + self._body + self._close
+
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
